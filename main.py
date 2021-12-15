6d5307ecadc9079bbcc078a390eec789a645e2e7
NaveTwizer <86551796+NaveTwizer@users.noreply.github.com>
2021-12-15 20:20:43 +0200

    Update main.py

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ MINUTES = get_minute()
 SECONDS = get_seconds()
 
 TARGET = "E:"
+# Make sure that you DO NOT have a hard drive named "E", if your disk on key target is called "F" make sure to change the target to "F:", else it will copy everything that's inside E, which you may not want if your disk on key target isn't called E.
 
 DESTINATION = f"C:/Users/someone/Desktop/Output_Folder/{YEAR}-{MONTH}-{HOUR}-{MINUTES}-{SECONDS}"
 #make sure to set up the destination to your choice. leave the year-month-hour-minutes-seconds if you want to make the folder look much better! :)
