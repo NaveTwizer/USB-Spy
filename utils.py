b0b9e9dd4a1a666cc8443444c1fe469b610f84d0
NaveTwizer <86551796+NaveTwizer@users.noreply.github.com>
2021-12-15 19:47:28 +0200

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,13 @@
+import datetime
+
+NOW = datetime.datetime.now()
+def get_year():
+    return NOW.year
+def get_month():
+    return NOW.month
+def get_hour():
+    return NOW.hour
+def get_minute():
+    return NOW.minute
+def get_seconds():
+    return NOW.second
